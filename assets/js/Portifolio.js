e6468b85691c226f55c3ebbac53c460b050eab3e
LucasVicentee <lh201612@gmail.com>
2025-10-24 22:21:17 -0300

    Adicionando o Scroll Reveal a todos elementos de cada seção

--- assets/js/Portifolio.js
+++ assets/js/Portifolio.js
@@ -66,8 +66,14 @@ ScrollReveal().reveal('#contato-links', {origin: "bottom"});
 ScrollReveal().reveal('#contato', {origin: "bottom"});
 
 // Revela elementos internos com efeitos suaves
+ScrollReveal().reveal('.texto-sobre p', {interval: 150, origin: 'left', delay: 400});
+ScrollReveal().reveal('#tecnologias-utilizadas h1', {interval: 150, origin: "top", delay: 400});
+ScrollReveal().reveal('.titulo-tecnologias h2', {interval: 150, origin: 'right', delay: 400});
 ScrollReveal().reveal('.card-tecnologia', { interval: 150, origin: 'bottom' });
+ScrollReveal().reveal('#certificados-obtidos h1', {interval: 150, origin: 'left', delay: 400});
 ScrollReveal().reveal('.formacao-container img', { interval: 150, origin: 'left', delay: 300 });
+ScrollReveal().reveal('#contato-links h1', {interval: 150, origin: 'top', delay: 400});
+ScrollReveal().reveal('#contato h1', {interval: 150, origin: 'top', delay: 400});
 ScrollReveal().reveal('.formacao-container p', { interval: 150, origin: 'right', delay: 400 });
 ScrollReveal().reveal('.contatos-container', {interval: 150, origin: "left", delay: 400});
 ScrollReveal().reveal('.entre-em-contato', {interval: 150, origin: "left", delay: 400});
